a78348ee1a67fdb5d023963dd366e9f7c22d450c
luzy <zhaoyang.lu@nldou.com>
2019-03-29 14:53:02 +0800

    add sms notificaiton channel

---
+++ src/Channels/Messages/SMSMessage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Channels\Messages;
+
+use Overtrue\EasySms\Message;
+use Overtrue\EasySms\Strategies\OrderStrategy;
+
+class SMSMessage extends Message
+{
+    // 定义本短信的网关使用策略，覆盖全局配置中的 `default.strategy`
+    protected $strategy = OrderStrategy::class;
+    // 定义本短信的适用平台，覆盖全局配置中的 `default.gateways`
+    protected $gateways = ['aliyun'];
+}
